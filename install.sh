8f38b5fa1286a19587894b72ecc6f91522ae8377
supasayajinduck <115066322+supasayajinduck@users.noreply.github.com>
2026-08-24 15:15:16 +0200

    Fix syntax error in install.sh

--- install.sh
+++ install.sh
@@ -247,14 +247,13 @@ Le script utilisera sudo uniquement pour installer les paquets."
     [[ -d "$HOME" ]] ||
         die "Le répertoire personnel n'existe pas : ${HOME}"
 
-    [[[ -d "$HOME"  "Le répertoire personnel"||
+    [[ -r "${SCRIPT_DIR}/${SCRIPT_NAME}" ]] ||
         die "Impossible de lire le script depuis : ${SCRIPT_DIR}"
 
     log_info "Utilisateur cible : $(id -un)"
     log_info "Répertoire personnel : ${HOME}"
     log_info "Dépôt Dotfiles : ${SCRIPT_DIR}"
 }
-
 ###############################################################################
 # Arguments
 ###############################################################################
