d34e7a7c09b1b21c901f708ad44423efbf012855
supasayajinduck <115066322+supasayajinduck@users.noreply.github.com>
2026-08-24 15:20:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,11 +21,17 @@ set -Eeuo pipefail
 # Variables globales
 ###############################################################################
 
-readonly SCRIPT_NAME="$(basename -- "$0")"
-readonly SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-readonly TIMESTAMP="$(date +'%Y%m%d-%H%M%S')"
-readonly BACKUP_ROOT="${HOME}/.dotfiles-backup"
-readonly BACKUP_DIR="${BACKUP_ROOT}/${TIMESTAMP}"
+readonly SCRIPT_NAME
+readonly SCRIPT_DIR
+readonly TIMESTAMP
+readonly BACKUP_ROOT
+readonly BACKUP_DIR
+
+SCRIPT_NAME="$(basename -- "$0")"
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+TIMESTAMP="$(date +'%Y%m%d-%H%M%S')"
+BACKUP_ROOT="${HOME}/.dotfiles-backup"
+BACKUP_DIR="${BACKUP_ROOT}/${TIMESTAMP}"
 
 DISTRO_ID=""
 DISTRO_NAME=""
